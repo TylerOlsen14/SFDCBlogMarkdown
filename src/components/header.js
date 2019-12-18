3eff459ee406dd2687ea8f6806292edb69b0666a
Tyler Olsen <Tyler.Olsen1@gmail.com>
2019-12-17 22:17:18 -0700

    added logo assets & css changes to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,19 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
+import logo from '../assets/LogoMakr_54VoFq copy.png'
 import './components.css'
 
 const Header = () => {
   return (
     <div className="header">
-      <h1>This is a header</h1>
+      <div className="logo">
+        <img src={logo} alt="Tucker, Finn, and co. Web Developers"/>
+      </div>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/About">About</Link>
+        <Link to="/Contact">Contact</Link>
+      </div>
     </div>
   )
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 
 import Layout from '../components/layout'
@@ -11,15 +10,6 @@ const Home = () => {
       <Layout>
         <PostList />
       </Layout>
-=======
-import React from "react"
-const Home = () => {
-  return (
-    <div>
-      <h1>
-        This is the home page
-      </h1>
->>>>>>> 26be6ca90bafb3b110d3029be83bc1b00bdf25ac
     </div>
   )
 }
